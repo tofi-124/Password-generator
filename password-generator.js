c912c524292b2fb618edaf704e76a6f82edaaa70
tofi-124 <tofikahm@yahoo.com>
2021-11-17 19:53:16 -0500

    Styles, scripts, and some html elements have been changed.
    Completely altered how this application generates the random password, and added the ability to store the produced password to the local machine with a custom file name.

--- password-generator.js
+++ password-generator.js
@@ -1,4 +1,8 @@
-function capitalLetter(str, length) {
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function passWord(param) {
   Letters = [
     "A",
     "B",
@@ -26,20 +30,6 @@ function capitalLetter(str, length) {
     "X",
     "Y",
     "Z",
-  ];
-  arr = str.split("");
-  let random;
-
-  for (i = 0; i < length; i++) {
-    random = Math.floor(Math.random() * length);
-    arr[random] = Letters[i];
-  }
-  arr = arr.join("");
-  return arr;
-}
-
-function smallLetter(str, length) {
-  Letters = [
     "a",
     "b",
     "c",
@@ -66,29 +56,19 @@ function smallLetter(str, length) {
     "x",
     "y",
     "z",
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    0,
   ];
-  arr = str.split("");
-  let random;
 
-  for (i = 0; i < length; i++) {
-    random = Math.floor(Math.random() * length);
-    arr[random] = Letters[i];
-  }
-  arr = arr.join("");
-  return arr;
-}
-
-function number(str, length) {
-  intg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  arr = str.split("");
-  let random;
-
-  for (i = 0; i < Math.floor(length / 2); i++) {
-    random = Math.floor((Math.random() * length) / 2);
-    arr[random] = intg[i];
-  }
-  arr = arr.join("");
-  return arr;
+  return Letters[param];
 }
 
 function pg() {
@@ -96,24 +76,58 @@ function pg() {
   var capitalChk = document.getElementById("capital");
   var smallChk = document.getElementById("small");
   var numberChk = document.getElementById("number");
-  var lenghtP = parseInt(prompt("Length of your password? ", ""));
-  var box = document.querySelector(".passwords");
+  var lenghtP = document.getElementById("length").value;
+  var box = document.getElementById("passWordtxt");
+  let capitalnum = true;
 
   for (i = 0; i < lenghtP; i++) {
-    str += "X";
+    if (capitalChk.checked && smallChk.checked && numberChk.checked == true) {
+      str += passWord(Math.floor(randomNumber(0, 62)));
+    } else if (capitalChk.checked && smallChk.checked == true) {
+      str += passWord(Math.floor(randomNumber(0, 52)));
+    } else if (capitalChk.checked && numberChk.checked == true) {
+      if (capitalnum == false) {
+        str += passWord(Math.floor(randomNumber(0, 25)));
+        capitalnum = true;
+      } else {
+        str += passWord(Math.floor(randomNumber(53, 59)));
+        capitalnum = false;
+      }
+    } else if (numberChk.checked && smallChk.checked == true) {
+      str += passWord(Math.floor(randomNumber(26, 59)));
+    } else if (smallChk.checked == true) {
+      str += passWord(Math.floor(randomNumber(26, 52)));
+    } else if (numberChk.checked == true) {
+      str += passWord(Math.floor(randomNumber(53, 59)));
+    } else if (capitalChk.checked == true) {
+      str += passWord(Math.floor(randomNumber(0, 25)));
+    }
   }
 
-  if (capitalChk.checked == true) {
-    str = capitalLetter(str, lenghtP);
-  }
-
-  if (smallChk.checked == true) {
-    str = smallLetter(str, lenghtP);
-  }
-
-  if (numberChk.checked == true) {
-    str = number(str, lenghtP);
-  }
-
+  box.value = str;
   box.innerHTML = str;
 }
+
+let saveFile = () => {
+  const siteTosave = document.getElementById("txtName");
+  var box = document.getElementById("passWordtxt");
+
+  let data =
+    "\r Site-Name: " + siteTosave.value + " \r\n " + "Password: " + box.value;
+
+  const textToBLOB = new Blob([data], { type: "text/plain" });
+  const sFileName = siteTosave.value + ".txt"; // The file to save the data.
+
+  let newLink = document.createElement("a");
+  newLink.download = sFileName;
+
+  if (window.webkitURL != null) {
+    newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+  } else {
+    newLink.href = window.URL.createObjectURL(textToBLOB);
+    newLink.style.display = "none";
+    document.body.appendChild(newLink);
+  }
+
+  newLink.click();
+};
